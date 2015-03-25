8975869c8077c313f1cbdc37d274c36fbc8baf66
JaviSG91 <javisago91@gmail.com>
2015-03-25 20:09:25 +0100

    easy

---
+++ GPSAlarm/src/com/example/gpsalarm/AlarmMap.java
@@ -0,0 +1,27 @@
+package com.example.gpsalarm;
+
+import java.io.IOException;
+import java.util.Date;
+
+import android.media.MediaPlayer;
+import android.os.Environment;
+import android.text.Editable;
+import android.util.Log;
+
+public class AlarmMap{
+	private Date d;
+	
+	
+	AlarmMap(){
+	
+	}
+	public Date getDate(){
+		
+		return d;
+	}
+	
+
+	
+
+
+}
